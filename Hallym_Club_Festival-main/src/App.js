081d5c5b537238202df06b9305fb17c9c9b1ed22
wjdwlsgh <wjdwlsgh02142@gmail.com>
2024-08-25 23:16:23 +0900

    .

---
+++ Hallym_Club_Festival-main/src/App.js
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import "./App.css";
+import { initVh } from "../src/utils/setVh";
+import React, { useEffect } from "react";
+import QR from "./pages/QR/QR.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Host from "./pages/Host/Host.jsx";
+import Start from "./pages/User/Start/Start.jsx";
+function App() {
+  useEffect(() => {
+    const cleanup = initVh(); //initVh() cleanup에 대입
+    return cleanup;
+  }, []);
+  return (
+    <>
+      <QR />
+
+      <div className="phone_size">
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Host" element={<Host />} />
+          <Route path="/Start" element={<Start />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Hallym_Club_Festival-main/src/pages/User/Start/Start.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from "../../../components/Background/Background";
+
+const Start = () => {
+  return (
+    <div>
+      <Background />
+      <div>화면을 터치해주세요.</div>
+    </div>
+  );
+};
+
+export default Start;
