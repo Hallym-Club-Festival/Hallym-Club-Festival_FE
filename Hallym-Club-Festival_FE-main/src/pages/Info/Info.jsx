ffa5b14d9ff91f728eb2c341bfd4ce16300458d0
wjdwlsgh <wjdwlsgh02142@gmail.com>
2024-08-26 00:29:21 +0900

    info 구현

--- Hallym-Club-Festival_FE-main/src/pages/Info/Info.jsx
+++ Hallym-Club-Festival_FE-main/src/pages/Info/Info.jsx
@@ -1,12 +1,69 @@
-import React from "react";
+import {React, useState} from "react";
 import "./Info.css";
 import Background2 from "../../components/Background/Background2";
+import { FaSearch } from "react-icons/fa";
 
 const Info = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+  
+  const [search, setSearch] = useState("")
+  
+  const onChange = (e) => {
+    setSearch(e.target.value)
+    
+  }
   return (
-    <div>
+    <div className="infoMain">
       <Background2 />
-      <h1>안내 페이지 입니다.</h1>
+      <div className="search">
+        <input type="text" className="search2" value={search} onChange={onChange} placeholder="검색어를 입력하세요"  />
+        <FaSearch className="icon" />
+        
+        <div className="AccordianMenu">
+        <div className="accordion-item">
+        <div className="accordion-title" onClick={() => handleToggle(0)}>
+          공지1
+          <span className={`arrow ${activeIndex === 0 ? 'up' : 'down'}`}>▲</span>
+        </div>
+        <div className={`accordion-content ${activeIndex === 0 ? 'show' : ''}`}>
+          내용1
+        </div>
+      </div>
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => handleToggle(1)}>
+          공지2
+          <span className={`arrow ${activeIndex === 1 ? 'up' : 'down'}`}>▲</span>
+        </div>
+        <div className={`accordion-content ${activeIndex === 1 ? 'show' : ''}`}>
+          내용2
+        </div>
+      </div>
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => handleToggle(2)}>
+          공지3
+          <span className={`arrow ${activeIndex === 2 ? 'up' : 'down'}`}>▲</span>
+        </div>
+        <div className={`accordion-content ${activeIndex === 2 ? 'show' : ''}`}>
+          내용3
+        </div>
+      </div>
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => handleToggle(3)}>
+          공지4
+          <span className={`arrow ${activeIndex === 3 ? 'up' : 'down'}`}>▲</span>
+        </div>
+        <div className={`accordion-content ${activeIndex === 3 ? 'show' : ''}`}>
+          내용4
+        </div>
+      </div>
+          
+        </div>
+      </div>
+    
     </div>
   );
 };
